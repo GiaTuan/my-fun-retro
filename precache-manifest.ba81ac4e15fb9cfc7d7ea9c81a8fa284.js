52225b62820fc39e54fb425ac43f2f6623610d52
tuan <adgtuan@gmail.com>
2020-10-28 18:31:18 +0700

    Updates

---
+++ precache-manifest.ba81ac4e15fb9cfc7d7ea9c81a8fa284.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1339d4dcab47129864409506ccb92b45",
+    "url": "/my-fun-retro/index.html"
+  },
+  {
+    "revision": "9345b4304d4c22454a72",
+    "url": "/my-fun-retro/static/css/main.0a682071.chunk.css"
+  },
+  {
+    "revision": "293bc03d5744fa4b2a6f",
+    "url": "/my-fun-retro/static/js/2.42303e38.chunk.js"
+  },
+  {
+    "revision": "15d1cee075267820b62f4314a3904c44",
+    "url": "/my-fun-retro/static/js/2.42303e38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9345b4304d4c22454a72",
+    "url": "/my-fun-retro/static/js/main.8b5f200d.chunk.js"
+  },
+  {
+    "revision": "6eb53d53496ab7fbbe3f",
+    "url": "/my-fun-retro/static/js/runtime-main.8aa88dc6.js"
+  }
+]);
